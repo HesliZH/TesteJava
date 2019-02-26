6ff17ba88cabfd49cb1b1b84efc5fd3a17c794a9
HesliZH <32403947+HesliZH@users.noreply.github.com>
2019-02-26 20:39:26 -0300

    SuperTrunfoClasse

---
+++ TesteJava/src/testejava/SuperTrunfoCarros.java
@@ -0,0 +1,12 @@
+package testejava;
+public class SuperTrunfoCarros {
+    private double Km;
+    private double Hp ;
+    private double ZeroToCem;
+    private double Cc;
+    private double Cilindros;
+    private double Peso;
+    private String Descricao;
+    private String PaisOrigem;
+    
+}
