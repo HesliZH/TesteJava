b516b421b7452b7c5033c424c0697607634b1a4d
HesliZH <32403947+HesliZH@users.noreply.github.com>
2019-02-26 19:49:36 -0300

    Opa

--- TesteJava/src/testejava/TesteJava.java
+++ TesteJava/src/testejava/TesteJava.java
@@ -5,7 +5,7 @@ public class TesteJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Opa!");
     }
     
 }
