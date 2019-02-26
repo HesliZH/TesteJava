876aa30404268e0ba87bd2ff8b2045223fe32caf
HesliZH <32403947+HesliZH@users.noreply.github.com>
2019-02-26 19:48:20 -0300

    Primeiro Commit Projeto Criado

---
+++ TesteJava/src/testejava/TesteJava.java
@@ -0,0 +1,11 @@
+package testejava;
+public class TesteJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
